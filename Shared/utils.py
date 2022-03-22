9c009749562e36ff6ec6901331260d4114927ded
ekeneattoh <ekeneattoh@gmail.com>
2022-03-22 16:53:57 +0100

    added other crud operations

--- Shared/utils.py
+++ Shared/utils.py
@@ -1,5 +1,5 @@
 import pymongo
-from pymongo.errors import WriteError
+from pymongo.errors import WriteError, PyMongoError
 
 client = pymongo.MongoClient(
     "mongodb+srv://onaide-dev:Aderonke!4@cluster0.v3ppc.mongodb.net/cocuisson-dev?retryWrites=true&w=majority")
@@ -31,6 +31,133 @@ def insert_one_into_db_collection(db_name: str, collection_name: str, data: dict
             result["status_message"] = "OK"
 
     except WriteError as e:
+        result["data"] = str(e)
+        result["status_message"] = "ERROR"
+
+    return result
+
+
+def find_one_in_collection(db_name: str, collection_name: str, search_query: dict) -> dict:
+    """
+
+    :param db_name: name of the db
+    :param collection_name: name of the db collection
+    :param search_query: search query
+    :return: dictionary of document found
+    """
+
+    result = {}
+
+    db = get_database(db_name=db_name)
+
+    collection = db[collection_name]
+
+    try:
+        result_document = collection.find_one(filter=search_query)
+        if result_document is not None:
+            result["data"] = result_document
+            result["status_message"] = "OK"
+        else:
+            result["data"] = "Document does not exist!"
+            result["status_message"] = "ERROR"
+    except PyMongoError as e:
+        result["data"] = str(e)
+        result["status_message"] = "ERROR"
+
+    return result
+
+
+def find_many_in_collection(db_name: str, collection_name: str, search_query: dict) -> dict:
+    """
+
+    :param db_name: name of the db
+    :param collection_name: name of the db collection
+    :param search_query: search query
+    :return: dictionary of documents found
+    """
+
+    result = {}
+    result_document_list = []
+
+    db = get_database(db_name=db_name)
+
+    collection = db[collection_name]
+
+    try:
+        result_documents = collection.find(filter=search_query)
+
+        for document in result_documents:
+            result_document_list.append(document)
+
+        if len(result_document_list) != 0:
+            result["data"] = result_document_list
+            result["status_message"] = "OK"
+        else:
+            result["data"] = "No documents found matching that query"
+            result["status_message"] = "ERROR"
+    except PyMongoError as e:
+        result["data"] = str(e)
+        result["status_message"] = "ERROR"
+
+    return result
+
+
+def update_one_set_in_collection(db_name: str, collection_name: str, search_query: dict, update_query: dict) -> dict:
+    """
+
+    :param db_name: name of the db
+    :param collection_name: name of the db collection
+    :param search_query: search query
+    :param update_query: modification dict
+    :return: dictionary of document found
+    """
+
+    result = {}
+
+    db = get_database(db_name=db_name)
+
+    collection = db[collection_name]
+
+    try:
+        result_document = collection.update_one(filter=search_query, update={"$set": update_query})
+        if result_document.modified_count == 1:
+            result["data"] = "OK"
+            result["status_message"] = "OK"
+        if result_document.matched_count != 1:
+            result["data"] = "No documents found matching that query"
+            result["status_message"] = "ERROR"
+
+    except WriteError as e:
+        result["data"] = str(e)
+        result["status_message"] = "ERROR"
+
+    return result
+
+
+def delete_one_in_collection(db_name: str, collection_name: str, search_query: dict) -> dict:
+    """
+
+    :param db_name: name of the db
+    :param collection_name: name of the db collection
+    :param search_query: search query
+    :return: dictionary of document found
+    """
+
+    result = {}
+
+    db = get_database(db_name=db_name)
+
+    collection = db[collection_name]
+
+    try:
+        result_document = collection.delete_one(filter=search_query)
+        if result_document is not None:
+            result["data"] = result_document
+            result["status_message"] = "OK"
+        else:
+            result["data"] = "Document does not exist!"
+            result["status_message"] = "ERROR"
+    except PyMongoError as e:
         result["data"] = str(e)
         result["status_message"] = "ERROR"
 
--- tests/utils-tests/test_utils.py
+++ tests/utils-tests/test_utils.py
@@ -8,17 +8,79 @@ class TestUtils(unittest.TestCase):
         self.data = {
             "firstname": "Ekene",
             "lastname": "Attoh",
-            "_id": "ekeneattoh@test.com"
+            "_id": "ekeneattoh@test.com",
+            "company_name": "cocuisson"
         }
+        self.db_name = "cocuisson-dev"
+        self.collection_name = "users"
+
+    def tearDown(self) -> None:
+        delete_one_in_collection(db_name=self.db_name, collection_name=self.collection_name,
+                                 search_query={"_id": "ekeneattoh@test.com"})
 
     def test_insert_one_into_db_collection(self):
         """
 
         :return:
         """
-        insert_test = insert_one_into_db_collection(db_name="cocuisson-dev", collection_name="users", data=self.data)
-
-        print(insert_test)
+        insert_test = insert_one_into_db_collection(db_name=self.db_name, collection_name=self.collection_name,
+                                                    data=self.data)
 
         assert insert_test["data"] == "OK"
 
+    def test_find_one_in_collection(self):
+        """
+
+        :return:
+        """
+
+        # add the data first
+        insert_one_into_db_collection(db_name=self.db_name, collection_name=self.collection_name,
+                                      data=self.data)
+
+        find_result = find_one_in_collection(db_name=self.db_name, collection_name=self.collection_name,
+                                             search_query={"_id": "ekeneattoh@test.com"})
+
+        assert find_result["status_message"] == "OK"
+
+    def test_find_many_in_collection(self):
+        """
+
+        :return:
+        """
+
+        # add the data first
+        insert_one_into_db_collection(db_name=self.db_name, collection_name=self.collection_name,
+                                      data=self.data)
+
+        data_two = {
+            "firstname": "Manon",
+            "lastname": "Picot",
+            "_id": "manonpicot@test.com",
+            "company_name": "cocuisson"
+        }
+
+        insert_one_into_db_collection(db_name=self.db_name, collection_name=self.collection_name,
+                                      data=data_two)
+
+        find_result = find_many_in_collection(db_name=self.db_name, collection_name=self.collection_name,
+                                              search_query={"company_name": "cocuisson"})
+
+        assert len(find_result["data"]) == 2
+
+        delete_one_in_collection(db_name=self.db_name, collection_name=self.collection_name,
+                                 search_query={"_id": "manonpicot@test.com"})
+
+    def test_update_one_into_db_collection(self):
+        """
+
+        :return:
+        """
+        insert_one_into_db_collection(db_name=self.db_name, collection_name=self.collection_name,
+                                      data=self.data)
+
+        update_result = update_one_set_in_collection(db_name=self.db_name, collection_name=self.collection_name,
+                                                     search_query={"_id": "ekeneattoh@test.com"},
+                                                     update_query={"company_name": "Onaide"})
+
+        assert update_result["data"] == "OK"
